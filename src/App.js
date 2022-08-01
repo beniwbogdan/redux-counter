f950be397b22c39b37f3fe4556f5bc4ebab8dafa
Bohdan Beniv <bohdanbeniv@gmail.com>
2022-08-01 15:49:17 +0200

    Create the buttons + & - with the initial state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+
+
+  const addOne=()=>{
+    
+
+  }
+
+  const minusOne=()=>{
+ 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{counter}</div>
+      <div>
+        <button onClick={()=>addOne()}>PLUS + </button>
+        <button onClick={()=>minusOne()}>MINUS - </button>
+      </div>
     </div>
   );
 }
